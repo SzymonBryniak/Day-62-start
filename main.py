560f1003132b7d7e2accc58aa7172982b6b1dff0
Szymon Bryniak <szymonbryniakproject@gmail.com>
2025-03-02 22:29:46 +0000

    Day 62 progress

--- main.py
+++ main.py
@@ -55,12 +55,13 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
+    data = "./cafe-data.csv"
     with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    return render_template('cafes.html',cafes=list_of_rows, data=data)
 
 
 if __name__ == '__main__':
